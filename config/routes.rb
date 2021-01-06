1c5b1abd47bb93356b6dffa9eceecb56a6453539
toyahatariku <76770034+toyahatariku@users.noreply.github.com>
2021-01-06 19:11:55 +0900

    Revert "メッセージ送信機能の実装"
    This reverts commit dece7b6e688f335cba23ea2000ff98c6e9420628.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
   root to: "rooms#index"
-  resources :rooms, only: [:new, :create] do
-    resources :messages, only: [:index, :create]
-  end
+  resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
 end
